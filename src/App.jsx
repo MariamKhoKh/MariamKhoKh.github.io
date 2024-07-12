3cb973294db949662d9880f62283ee9a1a9918fd
Khokhiashvili Mariami <142008382+MariamKhoKh@users.noreply.github.com>
2024-07-12 22:38:44 +0400

    pirveli

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+// import styles from "./App.module.css";
+// import { About } from "./components/About/About";
+// import { Contact } from "./components/Contact/Contact";
+// import { Experience } from "./components/Experience/Experience";
+// import { Hero } from "./components/Hero/Hero";
+// import { Navbar } from "./components/Navbar/Navbar";
+// import { Projects } from "./components/Projects/Projects";
+
+// function App() {
+//   return (
+//     <div className={styles.App}>
+//       <Navbar />
+//       <Hero />
+//       <About />
+//       <Experience />
+//       <Projects />
+//       <Contact />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import styles from "./App.module.css";
+import { About } from "./components/About/About";
+import { Contact } from "./components/Contact/Contact";
+import { Experience } from "./components/Experience/Experience";
+import { Skills } from "./components/Skills/Skills";
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Projects } from "./components/Projects/Projects";
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Experience />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
