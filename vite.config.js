5e5da2bed669efa61d503d145b73c1efbe2e83bc
Khokhiashvili Mariami <142008382+MariamKhoKh@users.noreply.github.com>
2024-07-12 22:55:37 +0400

    mariami

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/MariamKhoKh.github.io/", // Add this line
   plugins: [react()],
   css: {
     modules: {
